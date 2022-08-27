e29c236a509679ee9294b840d23e559a4c62cfbd
safi100 <safinafi12@gmail.com>
2022-08-27 16:02:41 +0300

    finish cart

---
+++ src/components/cart/Cart.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./cart.css"
+import Trash from "../../Assets/icon-delete.svg"
+import ProductImage from "../../Assets/image-product-1-thumbnail.jpg"
+const Cart = ({counterInsideCart, setCounterInsideCart, price}) => {
+  const HandleClearCart = () => {
+    setCounterInsideCart(0)
+  }
+  console.log(counterInsideCart);
+  return (
+    <div className='cart'>
+      <div className="cart__heading">Cart</div>
+      {(counterInsideCart == 0) && <div className="cart__empty">Your cart is empty.</div>}
+      {(counterInsideCart > 0) && <div className="cart__product">
+        <div className="cart-content__product">
+          <div className="cart-content__product__right">
+            <div className="product__img"><img src={ProductImage} alt="Product Image" /></div>
+            <div className="cart__product__description">
+              <div className="product__name">fall limited edition sneakers</div>
+              <div className="product__price"><span className='price__amount'>{`$${price}.00 x ${counterInsideCart}`}</span><span className='total'>{`$${(price * counterInsideCart)}.00`}</span></div>
+            </div>
+          </div>
+          <div className="left"><div className="trash" onClick={HandleClearCart}><img src={Trash} alt="Trash icon" /></div></div>
+        </div>
+        <button className='checkout'>Checkout</button>
+      </div>}
+    </div>
+  )
+}
+
+export default Cart
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from 'react'
+import Cart from '../cart/Cart'
 import Menu from "../../Assets/icon-menu.svg"
 import Logo from "../../Assets/logo.svg"
-import cart from "../../Assets/icon-cart.svg"
+import cart_icon from "../../Assets/icon-cart.svg"
 import user from "../../Assets/image-avatar.png"
 import "./navbar.css"
 import MobileNav from './MobileNav'
 import Blur from '../blur/Blur'
-const Navbar = () => {
-  const [open,setOpen] = useState(false)
+
+const Navbar = ({counterInsideCart,setCounterInsideCart, price}) => {
+  const [openMenu,setOpenMenu] = useState(false)
+  const [openCart, setOpenCart] = useState(false)
   return (
     <div className='navbar'>
+       {openCart && <Cart counterInsideCart={counterInsideCart} setCounterInsideCart={setCounterInsideCart} price={price}/>}
      <div className="wrapper">
       <div className="right">  
-        <div className="menu" onClick={()=>{ setOpen(true)}}><img src={Menu} alt="menu" /></div>
+        <div className="menu" onClick={()=>{ setOpenMenu(true)}}><img src={Menu} alt="menu" /></div>
         <a href='/' className="logo"><img src={Logo} alt="logo" /></a>
         <ul className='list'>
          <li className='list__li'>Collections</li>
@@ -23,16 +27,16 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="left">
-        <div className="cart">
-         <img src={cart} alt="cart" />
-         <span className='number_of_items'>3</span>
+        <div className="cart_notification" onClick={()=>{setOpenCart(!openCart)}}>
+         <img src={cart_icon} alt="cart" />
+         {(counterInsideCart > 0) && <span className='number_of_items'>{counterInsideCart}</span>}
          </div>
         <div className="user_img"><img src={user} alt="user" /></div>
       </div>
 
      </div>
-    <MobileNav status={open} setStatus={setOpen} />
-    {open && <Blur/>}
+    <MobileNav status={openMenu} setStatus={setOpenMenu} />
+    {openMenu && <Blur/>}
     </div>
   )
 }
