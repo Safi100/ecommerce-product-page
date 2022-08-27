74c502a1782af253b1eed9b129eac6ebc3d322ae
safi100 <safinafi12@gmail.com>
2022-08-27 16:03:18 +0300

    finish all price,and cart states

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./app.css"
 import Navbar from './components/navbar/Navbar'
 import ProductInfo from './components/productInfo/ProductInfo'
 import MobileSlider from './components/thumbnails/MobileSlider'
 
 const App = () => {
+  const [counterInsideCart , setCounterInsideCart] = useState(0)
+  const [price, setPrice] = useState(250)
+  const [discount_percentage, setDiscount_percentage] = useState(50)
+  const [priceAfterDiscount,setPriceAfterDiscount] = useState(0)
+  useEffect(()=>{
+    if((price - ((price * discount_percentage)/100)) < 0){
+      setPriceAfterDiscount(0)
+    }else{
+      setPriceAfterDiscount(price - ((price * discount_percentage)/100))
+    }
+  },[discount_percentage, price])
   return (
     <div>
-      <Navbar />
-      <MobileSlider />
-      <ProductInfo price={250} discount_percentage={50}/>
+      <Navbar
+      counterInsideCart={counterInsideCart}
+      setCounterInsideCart={setCounterInsideCart}
+      price={priceAfterDiscount}/>
+      <MobileSlider
+      />
+      <div className='wrapper'>
+       <ProductInfo
+        price={price}
+        discount_percentage={discount_percentage}
+        priceAfterDiscount={priceAfterDiscount}
+        setCounterInsideCart={setCounterInsideCart}
+       />
+      </div>
+      
     </div>
   )
 }
