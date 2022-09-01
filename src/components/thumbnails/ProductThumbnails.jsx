cc4886508bffae818498433b4f8f3a6f9e4ecebc
safi100 <safinafi12@gmail.com>
2022-09-02 01:39:27 +0300

    finish select thumbnails

--- src/components/thumbnails/ProductThumbnails.jsx
+++ src/components/thumbnails/ProductThumbnails.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Img1 from "../../Assets/image-product-1.jpg"
 import Img2 from "../../Assets/image-product-2.jpg"
 import Img3 from "../../Assets/image-product-3.jpg"
@@ -6,13 +6,17 @@ import Img4 from "../../Assets/image-product-4.jpg"
 import "./productThumbnails.css"
 const ProductThumbnails = () => {
     const Images = [Img1,Img2,Img3,Img4]
+    const [selectedIndex, setSelectedIndex] = useState(0)
+    useEffect(()=>{
+      console.log(selectedIndex);
+    },[selectedIndex])
   return (
     <div className='thumbnails'>
       <div className="Selected__photo">
-        <img src={Img1} alt="" />
+        <img src={Images[selectedIndex]} alt={`img${selectedIndex}`} />
       </div>
       <div className="group__photo">
-        {Images.map((img, index) => (<div className='img__div' key={index}><img src={img} alt={`img${index}`} /></div>))}
+        {Images.map((img, index) => (<div onClick={()=> setSelectedIndex(index)} className={`img__div ${(selectedIndex === index) ? "selected" : ""}`} key={index}><img className={`${(selectedIndex === index) ? "selected__inside" : ""}`} src={img} alt={`img${index}`} /></div>))}
       </div>
     </div>
   )
